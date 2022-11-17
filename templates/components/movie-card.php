dc71cef2bd11e7b2d232f5871ba1f3f69085679b
john <jjmiyao@gmail.com>
2022-11-16 22:31:03 -0800

    test deploybot

--- templates/components/movie-card.php
+++ templates/components/movie-card.php
@@ -2,6 +2,6 @@
 	<h2 class=" loud-voice name"><?php echo get_the_title(); ?></h2>
 	<h3 class="alert-voice year">Release Year: <?php the_field("release_year"); ?></h3>
 
-	<h3 class="alert-voice director">Director: <?php the_field("director"); ?></h3>
+	<h3 class="alert-voice director">Director</h3> <p class="normal-voice"><?php the_field("director"); ?></p>
 	<a href="<?php the_permalink(); ?>">Learn More</a>
 </movie-card>
