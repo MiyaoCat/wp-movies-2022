6c178495497711ca88bd5f7153ef4dc2350c1b63
john <jjmiyao@gmail.com>
2023-01-15 11:47:42 -0800

    test

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>My Favorite Flix</title>
+		<title>test</title>
 		<meta property="og:image" content=""/>
 
 		<title>test</title>
